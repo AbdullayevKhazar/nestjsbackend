ea05cf5b1415975acba273b8fa7f12b0c6c0d090
Abdullayev123 <xezeraa-bp216@code.edu.az>
2026-07-01 16:23:20 +0400

    feat : configuration and data base healt system

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { appConfig, databaseConfig, jwtConfig } from './config';
+
+import { envValidationSchema } from './config/env.validation';
+import { DatabaseModule } from './database';
+import { HealthModule } from './modules/health/health.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
       expandVariables: true,
+      load: [appConfig, databaseConfig, jwtConfig],
+      validationSchema: envValidationSchema,
     }),
+
+    DatabaseModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
---
+++ src/config/env.validation.ts
@@ -0,0 +1,21 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
+
+  PORT: Joi.number().default(5000),
+
+  API_PREFIX: Joi.string().default('api/v1'),
+
+  APP_NAME: Joi.string().required(),
+
+  MONGODB_URI: Joi.string().required(),
+
+  JWT_SECRET: Joi.string().required(),
+
+  JWT_EXPIRES_IN: Joi.string().required(),
+
+  JWT_REFRESH_SECRET: Joi.string().required(),
+
+  JWT_REFRESH_EXPIRES_IN: Joi.string().required(),
+});
